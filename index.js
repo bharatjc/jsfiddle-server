e31db2e48fd1f9aa19d5fd1dc29078585e9f0bac
Bharat Chaudhary <bhrt.7856@gmail.com>
2024-08-20 14:52:27 +0545

    new changes

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const authRoute = require("./route/auth");
 require("./config/database");
 const Joi = require("joi");
 const port = process.env.PORT;
+const User = require("./model/User");
+const nodemailer = require("nodemailer");
 
 app.use(cors());
 
@@ -14,6 +16,39 @@ app.get("/", function (req, res) {
 });
 
 app.use("/api", authRoute);
+
+app.post("/api/forgotpassword", function (req, res) {
+  const { email } = req.body;
+  User.findOne({ email: email }).then((user) => {
+    if (!user) {
+      return res.send({ status: "User didnot exist!" });
+    }
+    const JWT_SECRET_KEY = process.env.SECRET_KEY;
+    const token = jwt.sign(user, JWT_SECRET_KEY);
+    var transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "bikashchy184@gmail.com",
+        pass: "78$@sfAq",
+      },
+    });
+
+    var mailOptions = {
+      from: "bikashchy184@gmail.com",
+      to: email,
+      subject: "Reset your password",
+      text: `https://jsfiddleserver.onrender.com/api/resetpassword/${user._id}/${token}`,
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        return res.send({ status: "Success" });
+      }
+    });
+  });
+});
 
 app.listen(port, () => {
   console.log("server is running");
